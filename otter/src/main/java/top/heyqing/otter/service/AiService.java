eb94e473930b43648799a05c96105e2a45bfea98
heyqing <heyqings@163.com>
2025-06-15 18:04:01 +0800

    init

--- otter/src/main/java/top/heyqing/otter/service/AiService.java
+++
@@ -1,9 +0,0 @@
-package top.heyqing.otter.service;
-
-import java.util.List;
-
-public interface AiService {
-    List<String> getRecommendations(String userId, int limit);
-    String generateContent(String prompt);
-    boolean moderateContent(String content);
-} 
--- otter/src/main/java/top/heyqing/otter/service/WalletAuthService.java
+++
@@ -1,9 +0,0 @@
-package top.heyqing.otter.service;
-
-import top.heyqing.otter.dto.WalletAuthRequest;
-
-public interface WalletAuthService {
-    String generateNonce(String walletAddress);
-    boolean verifySignature(WalletAuthRequest request);
-    String generateToken(String walletAddress);
-} 
